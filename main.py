a8f614c283a061f0fb8ddb57a59df34855b173f0
gurnaldo <gurpreetsingh.panesar1996@gmail.com>
2021-07-31 18:33:32 +0530

    Frame Switch

--- main.py
+++ main.py
@@ -1,61 +1,66 @@
 from tkinter import *
 import tkinter.font as font
 from Libraries.videoPlayer import videoPlayer
-# from Libraries.serialData import serialData
-# from Libraries.GPIO import GPIO
-# from Libraries.inputExpander import inputExpander
- 
-# myTimer = serialData.serialData()         #Serial communication
-# myTimer.initLoop()
+    
+mainBackgroundColor = '#000CA4'
 
-# btn = GPIO.GPIOPins()
-# btn.initPins()
-# btn.beginReading()
+class myApp(Tk):
+    def __init__(self):
+        Tk.__init__(self)
+        self._frame = None
+        self.switch_frame(StartPage)
 
-# inputs = inputExpander.inputExpander()
-# inputs.init()
-# inputs.readDataLoop()
+    def switch_frame(self, frame_class):
+        new_frame = frame_class(self)
+        if self._frame is not None:
+            self._frame.destroy()
+        self._frame = new_frame
+        self._frame.grid(row=0, column=0)
 
-root = Tk()     #initialize the root window
-root.geometry("1024x600")
-root.config(bg='#000CA4')
+class StartPage(Frame):
+    def __init__(self, master):
+        Frame.__init__(self, master, bg=mainBackgroundColor)      
 
-#variables for widgets
-btnHeight=6
-btnWidth=12
-btnFont = font.Font(family="Segoe UI", size=8, weight='bold')
-textFont = font.Font(family="Segoe UI", size=22, weight='bold')
-btnTextColor = '#707070'
-btnColor = '#F3E82F'
+        #variables
+        btnHeight=6
+        btnWidth=12
+        btnFont = font.Font(family="Segoe UI", size=8, weight='bold')
+        textFont = font.Font(family="Segoe UI", size=22, weight='bold')
+        btnTextColor = '#707070'
+        btnColor = '#F3E82F'
 
-#definition of widgets
-# label_videoPannel = Label(root, height=28, width=109)
-label_videoPannel = Label(root)
+        btn_frame = Frame(self, highlightbackground='red', highlightthickness=3)
+        btn_frame.grid(row=0, column=0) 
+        
+        btn_fullScreen = Button(btn_frame, text="FULL SCREEN\nVIEW", height=btnHeight, width=btnWidth, font=btnFont, bg=btnColor, fg=btnTextColor,
+                                command=lambda: master.switch_frame(DiagnosticsPage))
+        btn_diagnostics = Button(btn_frame, text="DIAGNOSTICS", height=btnHeight, width=btnWidth, font=btnFont, bg=btnColor, fg=btnTextColor)
+        btn_moreParameters = Button(btn_frame, text="MORE\nPARAMETERS", height=btnHeight, width=btnWidth, font=btnFont, bg=btnColor, fg=btnTextColor)
+        btn_settings = Button(btn_frame, text="SETTINGS", height=btnHeight, width=btnWidth, font=btnFont, bg=btnColor, fg=btnTextColor)
 
-def openDiagnostics():
-    root.destroy()
-    import Pages.Diagnostics
+        btn_fullScreen.grid(row=0)
+        btn_diagnostics.grid(row=1)
+        btn_moreParameters.grid(row=2)
+        btn_settings.grid(row=3)
 
-btn_fullScreen = Button(text="FULL SCREEN\nVIEW", height=btnHeight, width=btnWidth, font=btnFont, bg=btnColor, fg=btnTextColor)
-btn_diagnostics = Button(text="DIAGNOSTICS", command=openDiagnostics, height=btnHeight, width=btnWidth, font=btnFont, bg=btnColor, fg=btnTextColor)
-btn_moreParameters = Button(text="MORE\nPARAMETERS", height=btnHeight, width=btnWidth, font=btnFont, bg=btnColor, fg=btnTextColor)
-btn_settings = Button(text="SETTINGS", height=btnHeight, width=btnWidth, font=btnFont, bg=btnColor, fg=btnTextColor)
-label_pressureText = Label(root, font=textFont, text="PRESSURE: 2200 PSI", bg='#000CA4', fg='#ffffff')
+        video_frame = Frame(self, highlightbackground='yellow', highlightthickness=3, bg=mainBackgroundColor)
+        video_frame.grid(row=0, column=1) 
 
-#placement of widgets
-paddingX = 25
-paddingY = 12
-label_videoPannel.grid(rowspan=3, row=0, column=1, columnspan=3, pady=(0,30))
-btn_fullScreen.grid(row=0, column=0, padx=paddingX, pady=(15, paddingY))
-btn_diagnostics.grid(row=1, column=0, padx=paddingX, pady=paddingY)
-btn_moreParameters.grid(row=2, column=0, padx=paddingX, pady=paddingY)
-btn_settings.grid(row=3, column=0, padx=paddingX, pady=paddingY)
-label_pressureText.grid(row=3, column=2, pady=(5, 0))
+        label_videoPannel = Label(video_frame)
+        label_pressureText = Label(video_frame, font=textFont, text="PRESSURE: 2200 PSI", bg=mainBackgroundColor, fg='#ffffff')
+        label_videoPannel.grid(row=0)
+        label_pressureText.grid(row=1)
 
-myVideoPlayer = videoPlayer.videoPlayer(label_videoPannel)   #use label as area to project video
-myVideoPlayer.playVideo()
+        myVideoPlayer = videoPlayer.videoPlayer(label_videoPannel)   #use label as area to project video
+        myVideoPlayer.playVideo()
 
-root.mainloop()     #read all elements in the window
+class DiagnosticsPage(Frame):
+    def __init__(self, master):
+        Frame.__init__(self, master, bg=mainBackgroundColor)
+        btn_fullScreen = Button(self, text="FULL SCREEN\nVIEW", command=lambda: master.switch_frame(StartPage)).grid()
 
-
-
+if __name__ == "__main__":
+    app = myApp()
+    app.geometry("1024x600")
+    app.config(bg=mainBackgroundColor)
+    app.mainloop()
